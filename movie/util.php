7e35a2bb65b4f2c6855e09588d6f9e4849ee9a1a
patklaey <pat.klaey@unifr.ch>
2014-10-10 17:49:03 +0200

    Util functions for movies

---
+++ movie/util.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Get the movie name from its ID
+ */
+function getMovieNameById( $id ){
+  // Join the id and node table on the entity_id/nid attribute and select the 
+  // nodes title
+  $query = db_select('node','node');
+  $query->join('field_data_field_movie_id','id','node.nid = id.entity_id');
+  $query->condition('id.field_movie_id_value',$id);
+  $query->fields('node',array('title'));
+  $results = $query->execute();
+  
+  if ( $results->rowCount() != 1 )
+    return "ERROR";
+  
+  return $results->fetchField();
+}
+
+?>
