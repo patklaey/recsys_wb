7e7c16d5ae5c6da5f6f9d00b6896a3a15b6a639f
patklaey <pat.klaey@unifr.ch>
2014-10-23 15:04:45 +0200

    Ratings with Book name and link to book or movie

--- util.php
+++ util.php
@@ -18,4 +18,22 @@ function getMovieNameById( $id ){
   return $results->fetchField();
 }
 
+/**
+ * Get the book name from its ID
+ */
+function getBookNameById( $id ){
+  // Join the id and node table on the entity_id/nid attribute and select the 
+  // nodes title
+  $query = db_select('node','node');
+  $query->join('field_data_field_book_id','id','node.nid = id.entity_id');
+  $query->condition('id.field_book_id_value',$id);
+  $query->fields('node',array('title'));
+  $results = $query->execute();
+  
+  if ( $results->rowCount() != 1 )
+    return "ERROR";
+  
+  return $results->fetchField();
+}
+
 ?>
