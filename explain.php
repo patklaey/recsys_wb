dea6ebf48a39367abe628e4249a7286fb80369fd
patklaey <pat.klaey@unifr.ch>
2014-12-05 15:35:39 +0100

    Explaining euclidean similarity

--- explain.php
+++ explain.php
@@ -146,7 +146,7 @@ function recsys_wb_explain_cosine( $marking = 0 ) {
   $explanation .= "For the cosine similarity the products or users are seen as 
  vectors in n-dimensional space. The similarity of two products or users 
  (vectors) is given by the angle that they form: ";
-  $cosine = '\cos( \overrightarrow{x},\overrightarrow{y}) = {\overrightarrow{x}
+  $cosine = 'Cosine( \overrightarrow{x},\overrightarrow{y}) = {\overrightarrow{x}
 \bullet \overrightarrow{y} \over |\overrightarrow{x}| \times |\overrightarrow{y}
 |}';
   $explanation .= mathBlock($cosine);
@@ -155,7 +155,7 @@ where 0 indicates no similarity and 1 indicates absolute similarity. <br/> Let's
  have a look at a concrete example. Take the table below: ";
   $explanation .= recsys_wb_get_example_table( MARK_USER2USER );
   $explanation .= "The similarity between User 1 and Bob is:";
-  $example = '\cos(User 1,Bob) = {4 \times 5 + 1 \times 2 + 2 \times 3 + 4 '
+  $example = 'Cosine(User 1,Bob) = {4 \times 5 + 1 \times 2 + 2 \times 3 + 4 '
    . '\times 4 \over \sqrt{4^2+1^2+2^2+4^2} \times \sqrt{5^2+2^2+3^2+4^2} } = '
    . '0.98';
   $explanation .= mathBlock($example);
@@ -184,25 +184,79 @@ mathInline('\overline{Y}') . "is the average rating of " . mathInline('X') .
 where 1 means strong positive correlation (similarity), 0 means no correlation (
 no similarity) and -1 means strong negative correlation (dissimilarity).<br/>
 Let's have a look at a concrete example. Take the table below: ";
-  $explanation .= recsys_wb_get_example_table( MARK_ITEM2ITEM );
-  $explanation .= "To calculate the similarity between Item 1 and Item 5 we 
+
+  // If user2user is marked (or nothing)
+  if ( $marking == MARK_USER2USER || $marking == 0 ) {
+    $explanation .= recsys_wb_get_example_table( MARK_USER2USER );
+    $explanation .= "To calculate the similarity between User 1 and Bob we 
+first calculate " . mathInline('\overline{User 1}') . " and " 
+. mathInline('\overline{Bob}') . ".<br/>";
+    $precompute = '\overline{User 1} = {4+1+2+4 \over 4} = 2.75';
+    $explanation .= mathBlock($precompute);
+    $precompute = '\overline{Bob} = {5+2+3+4 \over 4} =  3.5';
+    $explanation .= mathBlock($precompute);
+    $sum = '(1.75 \times 1.5) + (-1.75 \times -1.5) + ( -0.75 \times -0.5) + '
+      . '(1.75 \times 0.5 )';
+    $sqrt_x = '\sqrt{ 3.0625 + 3.0625 + 0.5625 + 3.0625 }';
+    $sqrt_y = '\sqrt{ 2.25 + 2.25 + 0.25 + 0.25 }';
+    $example = 'Pearson(User 1,Bob) = { ' . $sum . ' \over ' . $sqrt_x 
+      . ' \times ' . $sqrt_y . '} = 0.82';
+    $explanation .= mathBlock($example);
+    $explanation .= "As we can see a value of 0.82 means that there is a  
+positive correlation (and therefore similarity) between User 1 and Bob. To 
+apply the pearson similarity to items the process is exactly the same. You just 
+measure the correlation between the ratings of the given items instead of the 
+ratings of the given users (just invert the matrix).";
+  }
+  else {
+    $explanation .= recsys_wb_get_example_table( MARK_ITEM2ITEM );
+    $explanation .= "To calculate the similarity between Item 1 and Item 5 we 
 first calculate " . mathInline('\overline{Item 1}') . " and " 
 . mathInline('\overline{Item 5}') . ".<br/>";
-  $precompute = '\overline{Item1} = {4+2+5+3 \over 4} = 3.5';
-  $explanation .= mathBlock($precompute);
-  $precompute = '\overline{Item5} = {4+2+4+3 \over 4} = 3.25';
-  $explanation .= mathBlock($precompute);
-  $sum = '(0.5 \times 0.75) + (-1.5 \times -1.25) + ( 1.5 \times 0.75) + (-0.5 \times -0.25 )';
-  $sqrt_x = '\sqrt{ 0.25 + 2.25 + 2.25 + 0.25 }';
-  $sqrt_y = '\sqrt{ 0.5625 + 1.5625 + 0.5625 + 0.0625 }';
-  $example = 'Pearson(Item1,Item5) = { ' . $sum . ' \over ' . $sqrt_x 
-. ' \times ' . $sqrt_y . '} = 0.94';
-  $explanation .= mathBlock($example);
-  $explanation .= "As we can see a value of 0.98 means that there is a strong 
+    $precompute = '\overline{Item1} = {4+2+5+3 \over 4} = 3.5';
+    $explanation .= mathBlock($precompute);
+    $precompute = '\overline{Item5} = {4+2+4+3 \over 4} = 3.25';
+    $explanation .= mathBlock($precompute);
+    $sum = '(0.5 \times 0.75) + (-1.5 \times -1.25) + ( 1.5 \times 0.75) + '
+      . '(-0.5 \times -0.25 )';
+    $sqrt_x = '\sqrt{ 0.25 + 2.25 + 2.25 + 0.25 }';
+    $sqrt_y = '\sqrt{ 0.5625 + 1.5625 + 0.5625 + 0.0625 }';
+    $example = 'Pearson(Item1,Item5) = { ' . $sum . ' \over ' . $sqrt_x 
+      . ' \times ' . $sqrt_y . '} = 0.94';
+    $explanation .= mathBlock($example);
+    $explanation .= "As we can see a value of 0.98 means that there is a strong 
 positive correlation (and therefore similarity) between Item 1 and Item 5. To 
 apply the pearson similarity to users the process is exactly the same. You just 
 measure the correlation between the ratings of the given users instead of the 
 ratings of the given items (just invert the matrix).";
+  }
+  return $title . $explanation . "</div>";
+}
+
+/**
+ * Shortly explain the euclidean similarity
+ */
+function recsys_wb_explain_euclidean( $marking = 0 ) {
+  $title = "<h2>Euclidean similarity</h2>";
+  $explanation = "<div class='tex2jax'>";
+  $explanation .= "For the euclidean similarity the products or users are seen 
+as points in n-dimensional space. The similarity of two products or users 
+(points) is given by their distance:";
+  $euclidean = 'Euclidean(X,Y) = \sqrt{ \sum_{i=1}^{n}(X_i - Y_i)^2 }';
+  $explanation .= mathBlock($euclidean);
+  $explanation .= "The smaller the distance, the bigger the similarity between 
+the two users or products. <br/>Let's have a look at a concrete example. Take 
+the table below: ";
+  $explanation .= recsys_wb_get_example_table( MARK_USER2USER );
+  $explanation .= "The similarity between User 1 and Bob is:";
+  $example = 'Euclidean(User1,Bob) = \sqrt{(-1)^2 + (-1)^2 + (-1)^2 + 0^2 } = ';
+  $example .= " 1.73";
+  $explanation .= mathBlock($example);
+  $explanation .= "At a first glance, a value of 1.73 seems to be a lot, but the
+the distnace between Bob and User 2 and User 3 is 4.24 and 2.65 respectively. To
+ apply the euclidean similarity to items the process is exactly the same. You 
+just measure the distance between the ratings of the items instead of the 
+ratings of the users (just invert the matrix)";
   return $title . $explanation . "</div>";
 }
 ?>
