087ffac41713efb06eb1d2f08fafa706bc71ba4a
patklaey <pat.klaey@unifr.ch>
2014-12-23 14:35:13 +0100

    Finished TF-IDF explanation

--- CB.php
+++ CB.php
@@ -40,7 +40,31 @@ the word " . mathInline("w") . " occurs in document " . mathInline("d") . ", "
 . mathInline("d") . " except word " . mathInline("w") . ", " . mathInline("N")
 . " is the total number of documents and " . mathInline("n(w)") . " is the 
 number of documents in which the word " . mathInline("w") . " occurs at least 
-once.";
+once.<br/>";
+  $explanation .= "Consider the following two sentences:";
+  $explanation .= "<code>This is some text about a text recommender system
+</code>";
+  $explanation .= "<code>Content based recommender system are cool</code>";
+  $explanation .= "Let's have a look at an exmple: " 
+. mathInline('TF\text{-}IDF(text,S_1)') . ":";
+  $text_tf = 'TF(text,S_1) = {2 \over 2} = 1';
+  $text_idf = 'IDF(text) = \log{2 \over 1} = 0.301';
+  $text_tfidf = 'TF(text,S_1) \times IDF(text) = 1 \times 0.301 = 0.301';
+  $explanation .= mathBlock($text_tf) . mathBlock($text_idf)
+. mathBlock($text_tfidf) . mathBlock('TF\text{-}IDF(text,S_1) = 0.301');
+
+  $explanation .= "Let's caluclate " 
+. mathInline('TF\text{-}IDF(recommender,S_2)') . ":";
+  $recommender_tf = 'TF(recommender,S_2) = {1 \over 1} = 1';
+  $recommender_idf = 'IDF(recommender) = \log{2 \over 2} = 0';
+  $recommender_tfidf = 'TF(recommender,S_2) \times IDF(recommender) = 1 \times
+0 = 0';
+  $explanation .= mathBlock($recommender_tf) . mathBlock($recommender_idf) 
+. mathBlock($recommender_tfidf) 
+. mathBlock('TF\text{-}IDF(recommender,S_2) = 0');
+  $explanation .= "As we can see, the TF-IDF value for the word 'recommender' in
+sentence 2 is 0. In fact, every word which occurs in every document has an 
+TF-IDF value of 0 (as " . mathInline('IDF(word) = \log{N \over N } = 0') . ").";
   return $title . $explanation . "</div>";
 }
 ?>
