de87c56aab8cdb43c5f85756d3a89cdabe5bde61
patklaey <pat.klaey@unifr.ch>
2015-01-08 11:17:50 +0100

    Finishing NDCG explanation

--- evaluation_explain.php
+++ evaluation_explain.php
@@ -164,16 +164,16 @@ function recsys_wb_explain_rmse() {
   $explanation .= "The Mean Absolut Error metric determines, by how much the 
 average prediction is wrong. The formula is quite similar to the Mean Absolute 
 Error, but it penalizes big errors over small ones:";
-  $rmse = 'RMSE(r) = \sqrt{\sum_{i=0}^I {\sum_{u=0}^U {  (pred(r,u,i) - 
-rating(u,i))^2 }} \over I + U}';
+  $rmse = 'RMSE(r) = \sqrt{\sum_{i=0}^I {  (pred(r,u,i) - 
+rating(u,i))^2 } \over I + U}';
   $explanation .= mathBlock($rmse);
   $explanation .= "Where " . mathInline('I') . " is the set of items in the test 
-set, " . mathInline('U') . " is the set of users in the test set, "
-. mathInline('pred(r,u,i)') . " is the prediction of the recommender algorithm "
-. mathInline('r') . " for the user " . mathInline('u') . " on the item "
-. mathInline('i') . " and " . mathInline('rating(u,i)') . " is the rating of 
-user " . mathInline('u') . " on item " . mathInline('i') . ".<br/>Consider the 
-following simple example (1 item, 5 users):";
+set, ". mathInline('pred(r,u,i)') . " is the prediction of the recommender 
+algorithm " . mathInline('r') . " for the user " . mathInline('u') . " on  
+item with index " . mathInline('i') . " and " . mathInline('rating(u,i)') . " is
+ the actual rating of user " . mathInline('u') . " on item with index" 
+. mathInline('i') . ".<br/>Consider the following simple example (1 item, 5 
+users):";
   $explanation .= recsys_wb_evaluation_example_accuracy_table();
   $explanation .= "So the RSME would be: ";
   $rsme_example = '\sqrt{(4-3)^2 + (2-4)^2 + (3-3)^2 + (5-4)^2 + (2-3)^2 \over';
@@ -225,8 +225,23 @@ function recsys_wb_explain_ndgc() {
   $explanation .= "The Normalized Discounted Normalized Discounted Cumulative 
 Gain metric determines how correct the algorithms predictions are in respect of 
 the rank the items appear in. It compares the ranked list of the recommender 
-algorithm with an perfectly sorted list of these items<sup>$ndcg_citation_link
-</sup>.";
+algorithm with an perfectly sorted list of the items<sup>$ndcg_citation_link
+</sup>. The result of this comparision will be between 0 and 1 where a perfectly 
+ sorted list will get 1 and an inversed sorted list will get 0. So the closer to
+ 1 the result is, the better the algorithm sorts the items. The formula is as 
+follows:";
+  $ndcg = 'NDCG(r) = {DCG(r) \over DCG(L_{perfect})}';
+  $dcg = 'DCG(r) = \sum_{i=0}^I { rating(u,i) \times discount(i)}';
+  $discount = 'discount(i) = {1 \over max(1,\log_2{i})}';
+  $explanation .= mathBlock($ndcg) . mathBlock($dcg) . mathBlock($discount);
+  $explanation .= "Where " . mathInline('r') . " is the recommender algorithm, "
+. mathInline('L_{perfect}') . " is a perfectly sorted item list by users ratings
+, " . mathInline('I') . " is the set of items in the test set and "
+. mathInline('rating(u,i)') . " is the actual rating of user " . mathInline('u')
+. " on item with index " . mathInline('i') . ".<br/>So as one can see, the first
+ ratings are far more important than the last ones (caused by the logarithmic 
+discount). This is because the first items are much more likely to be selected 
+by a user so they should have more impact on the result.";
   return $title . $explanation . "</div>";
 }
 ?>
